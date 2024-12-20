79d8445fdc209b4f27d2bcf351a7bf401edcc0ef
taka4655 <taku.enginner@gmail.com>
2024-12-20 20:47:41 +0900

    ファビコンを無視

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  # ファビコンのルートを無視する
+  get "/favicon.ico", to: ->(_) { [204, {}, []]}
+
 
   root "application#hello"
 end
